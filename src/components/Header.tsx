ff9455751e5239ac5e8be8096038c39172e40fad
Kellen Parker <kellenrparker@gmail.com>
2022-06-22 16:29:09 -0500

    changes to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
 
     return (
         <div className="header">
-            <p className="title">Kellen Parker</p>
+            <p className="title">kp</p>
             <div className="btn-group" role="group" aria-label="header buttons">
                 <button type="button" className="btn-head btn-left">Home</button>
                 <button type="button" className="btn-head btn-secondary">About Me</button>

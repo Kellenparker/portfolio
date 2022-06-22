1fd0d9fdd1b80654c9a92f2ffbb4904e4a6b5c52
Kellen Parker <kellenrparker@gmail.com>
2022-06-22 11:42:00 -0500

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home'
+import Header from './components/Header';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Header/>
+				<Routes>
+					<Route path="/" element={<Home/>}/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles/Header.css'
+
+function Header() {
+
+    return (
+        <div className="header">
+            <p className="title">Kellen Parker</p>
+            <div className="btn-group" role="group" aria-label="header buttons">
+                <button type="button" className="btn-head btn-left">Home</button>
+                <button type="button" className="btn-head btn-secondary">About Me</button>
+                <button type="button" className="btn-head btn-secondary">Projects</button>
+                <button type="button" className="btn-head btn-secondary">Resume</button>
+                <button type="button" className="btn-head btn-right">Contact</button>
+            </div>
+        </div>
+    );
+
+}
+
+export default Header;

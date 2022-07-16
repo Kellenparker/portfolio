b907b0138651ceb034910b88774f81bed0f2e092
Kellen Parker <kellenrparker@gmail.com>
2022-07-16 11:52:23 -0500

    added resume responsiveness

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "./styles/Resume.css";
 import res from "./pdf/kellen_parker_resume.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-    "https://raw.githubusercontent.com/Kellenparker/portfolio/main/src/components/pdf/kellen_parker_resume.pdf";
+const resLink = "https://raw.githubusercontent.com/Kellenparker/portfolio/main/src/components/pdf/kellen_parker_resume.pdf";
 
-function ResumeNew() {
+function Resume() {  
+    const [width, setWidth] = useState(1200);
+
+    useEffect(() => {
+      setWidth(window.innerWidth);
+    }, []);
+    
     return (
         <section className="resume-section">
             <section className="resume">
-                <Document file={resumeLink} className="resume-pdf">
-                    <Page pageNumber={1} scale={1.5} />
+                <Document file={resLink} className="resume-pdf">
+                    <Page pageNumber={1} scale={(width / 1000.0) + .3 > 1.5 ? 1.5 : (width / 1000.0) + .3} />
                 </Document>
             </section>
             <div className="download">
@@ -24,4 +29,4 @@ function ResumeNew() {
     );
 }
 
-export default ResumeNew;
+export default Resume;
